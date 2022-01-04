eb0ef4946c38b962e8c22414f77bd9f37c66b52c
Ramit Chandra <ramit.chandra@yahoo.co.in>
2022-01-04 15:08:50 +0530

    Basic Structure

---
+++ src/main/java/com/lt/crs/app/Application.java
@@ -0,0 +1,13 @@
+package com.lt.crs.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
