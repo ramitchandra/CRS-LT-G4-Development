5a0cb362c45e99c1fe4bdcb36f52658140b7e57b
radha <radharamyaa11@gmail.com>
2022-01-05 14:44:52 +0530

    student implementation

--- src/main/java/com/lt/crs/controllers/StudentController.java
+++ src/main/java/com/lt/crs/controllers/StudentController.java
@@ -1,7 +1,9 @@
 package com.lt.crs.controllers;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.core.MediaType;
 
@@ -13,41 +15,52 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lt.crs.bean.Course;
+import com.lt.crs.business.CourseHandler;
 import com.lt.crs.bean.Student;
 import com.lt.crs.business.StudentHandler;
 
 @RestController
 public class StudentController {
 	
+	Map<Integer,List> addedCourses = new HashMap<Integer,List>();
+	
 	@Autowired
 	StudentHandler studentHandlerImpl;
 	
-	List<String> courseList = new ArrayList<String>();	
+	@Autowired
+	CourseHandler courseHandlerImpl;
 	
 	@RequestMapping(value = "student/registerCourse/{id}", produces = MediaType.APPLICATION_JSON, method = RequestMethod.GET)
-	public List<String> registerCourse() {
-		return courseList;
+	public List<String> registerCourse(@PathVariable int id) {
+		return addedCourses.get(id) ;
 	
 	}
 
 	@RequestMapping(value = "student/addCourse/{id}/{Course}", produces = MediaType.APPLICATION_JSON, method = RequestMethod.PUT)
-	public void addCourse(@PathVariable String Course) {
+	public Map<Integer, List> addCourse(@PathVariable String Course, @PathVariable int id) {
+		List<String> courseList = new ArrayList<String>();
+		if(addedCourses.get(id) != null) {
+		courseList = addedCourses.get(id);
+	}
 	
-	List<Course> courseCatalog = new ArrayList<Course>();	//AVAILABLE COURSES
+	List<Course> courseCatalog = courseHandlerImpl.getCourseList();	
 	for(Course c : courseCatalog) {
 		if(c.getCourseName().equalsIgnoreCase(Course)) {
 			if(!courseList.contains(Course))
 				courseList.add(Course);
-			
-		}
-	}
+		}		
+	}	
+	addedCourses.put(id, courseList);
+	return addedCourses;
 	}
 
-	@RequestMapping(value = "/student/dropCourse/{id}/{Course}", produces = MediaType.APPLICATION_JSON, method = RequestMethod.PUT)
-	public void dropCourse(@PathVariable String Course) {
-		if(!courseList.contains(Course))
+	@RequestMapping(value = "/student/dropCourse/{id}/{Course}", produces = MediaType.APPLICATION_JSON, method = RequestMethod.DELETE)
+	public Map<Integer, List> dropCourse(@PathVariable String Course, @PathVariable int id) {
+		List<String> courseList = addedCourses.get(id);
+		if(courseList.contains(Course))
 			courseList.remove(Course);
-		
+		addedCourses.put(id, courseList);
+		return addedCourses;
 	}
 	
 	@RequestMapping(value = "/student/addStudent", produces = MediaType.APPLICATION_JSON, method = RequestMethod.POST)
