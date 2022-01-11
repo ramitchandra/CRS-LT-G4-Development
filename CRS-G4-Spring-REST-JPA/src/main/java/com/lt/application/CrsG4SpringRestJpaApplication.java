518ba6d5390b8852770ba240762a48aca9ff9a78
radha <radharamyaa11@gmail.com>
2022-01-11 14:29:57 +0530

    Spring Rest Jpa Project added

---
+++ CRS-G4-Spring-REST-JPA/src/main/java/com/lt/application/CrsG4SpringRestJpaApplication.java
@@ -0,0 +1,13 @@
+package com.lt.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrsG4SpringRestJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrsG4SpringRestJpaApplication.class, args);
+	}
+
+}
