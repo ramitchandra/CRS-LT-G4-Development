f16f2bc1f2054581a81e5b1db9c64d53548becb8
purnima <devipurnima992@gmail.com>
2022-01-29 15:57:09 +0530

    add course and delete course

--- CRS-LT-G4-Angular/src/app/app.module.ts
+++ CRS-LT-G4-Angular/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
-    LoginComponent
+    LoginComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,CourseComponent]
+  
 })
 export class AppModule { }
---
+++ CRS-LT-G4-Angular/src/app/course/course.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../course';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit {
+  courseArray: Array<Course> = new Array();
+
+  model = new Course(1,'', true, 0, 0);
+  updateFlag: boolean = false;
+  updateData: any = {};
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  // Craete & List The Customer 
+
+  createCourse() {
+
+    //console.log("customer creation here-->");
+
+    // Add customer in Customer Array using push event.
+
+
+
+    this.courseArray.push(new Course(this.model.courseId,this.model.courseName, this.model.courseAvailable, this.model.onlineAmount, this.model.offlineAmount));
+
+    console.log(JSON.stringify(this.courseArray));
+    this.model.courseName = ""
+    this.model.courseAvailable = true
+    this.model.onlineAmount = 0
+    this.model.offlineAmount = 0
+  }
+
+
+  deleteCustomer(data, index) {
+    this.courseArray.splice(index, 1);
+  }
+}
