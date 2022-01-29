06ee498d2771844a932f1fb287d90d6f67b270d9
Naman Kansal <namankansal7@gmail.com>
2022-01-29 13:13:53 +0530

    Angular Admin Service and Login Component

--- CRS-LT-G4-Angular/src/app/app.module.ts
+++ CRS-LT-G4-Angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    AdminComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
