4b90b710773d526c4797910231491b212621a2ba
purnima <devipurnima992@gmail.com>
2022-01-29 17:54:06 +0530

    course add or delete

--- CRS-LT-G4-Angular/src/app/course/course-service.service.ts
+++ CRS-LT-G4-Angular/src/app/course/course-service.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Course } from './course';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseServiceService {
+  
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  public save(createCourse: Course){
+    this.http.post<Course>('http://localhost:7081/addCourse',createCourse, {observe: 'response'}).subscribe(result => {
+      console.log("The request was a success!")
+      },
+      () => {console.log("There was an Error")});
+  }
 }
