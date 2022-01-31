2f2bd4602634b41492205c63bed3d1829ad5abd1
Ramit Chandra <ramit.chandra@yahoo.co.in>
2022-01-31 12:36:07 +0530

    login validation

--- CRS-LT-G4-Angular/src/app/login/login.component.ts
+++ CRS-LT-G4-Angular/src/app/login/login.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  msg:boolean = false;
   userName:string;
   password:string;
   constructor(private loginService:LoginService,private router: Router) { }
@@ -25,9 +25,20 @@ export class LoginComponent implements OnInit {
     this.loginService.userLogin(userLogin).subscribe(
       response => {
         console.log(response);
-        this.router.navigate(['/admin']);
+        if(response && response.msg === 'User Logged In'){
+          this.router.navigate(['/admin']);
+        } else {
+          this.msg = true;
+          this.userName = "";
+          this.password = "";
+          this.router.navigate(['/login']);
+        }
       }
      );
+  }
+
+  closeAlert(){
+    this.msg = false;
   }
 
 }
